07bafaf216b964b52f4800514e87547e64144764
shangpf1 <208418427@qq.com>
2018-09-09 00:34:46 +0800

    while语句

---
+++ 2018-08/TestWhile.java
@@ -0,0 +1,23 @@
+package lianxi;
+
+public class TestWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		int num = 1;
+//		while(num<=10){
+//			System.out.println("第"+num+"次循环");
+//			num++;
+//		}
+		
+		int j = 1;
+		do{
+			System.out.println("第"+j+"次循环");
+			j++;
+		}while(j<=5);
+		
+
+	}
+
+}
