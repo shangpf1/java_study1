e20016363e9488dce972b34e00c36567b0e86c94
shangpf1 <208418427@qq.com>
2018-09-09 00:20:19 +0800

    socre

---
+++ 2018-08/Score.java
@@ -0,0 +1,18 @@
+package lianxi;
+
+public class Score {
+
+	public static void main(String[] args) {
+		int Score = 60;
+		if(Score>90){
+			System.out.println("优秀");
+			
+		}else if(Score>60&Score<90){
+			System.out.println("良好");
+		}else{
+			System.out.println("不及格！");
+		}
+
+	}
+
+}
