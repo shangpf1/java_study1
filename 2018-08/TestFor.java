106f19cebef980ae931804b28718e71251b2090c
shangpf1 <208418427@qq.com>
2018-09-09 08:11:14 +0800

    for循环语句

---
+++ 2018-08/TestFor.java
@@ -0,0 +1,15 @@
+package lianxi;
+
+public class TestFor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int value = 1;
+		for(int i=3;i>0;i--){
+			value*=i;
+		}
+		System.out.println("value:"+value);
+
+	}
+
+}
