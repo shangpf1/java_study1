335c244748b854dc6495c89a59f9b7e96e346bd8
shangpf1 <208418427@qq.com>
2018-09-17 22:03:25 +0800

    Boy-封装1

---
+++ 2018-08/Boy.java
@@ -0,0 +1,44 @@
+package lianxi;
+
+public class Boy {
+	
+	//属性
+	private String name;
+	private String sex;
+	private int age;
+	private String addr;
+	
+	//方法
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(String name){
+		this.name=name;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getAddr() {
+		return addr;
+	}
+
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+    
+}
