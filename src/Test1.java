fa83a7f4d486d35763d7c23b91637d09fef98d45
shangpf1 <208418427@qq.com>
2019-06-27 23:35:27 +0800

    测试

---
+++ src/Test1.java
@@ -0,0 +1,14 @@
+public class Test1 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1;i <=100;i++){
+
+            if (i%2!=0){
+                System.out.println(i);
+                sum += i;
+            }
+
+        }
+        System.out.println(sum);
+    }
+}
